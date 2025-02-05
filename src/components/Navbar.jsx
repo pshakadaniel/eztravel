c3571c06ee0d573d4e478cfb4c98974bda639d33
pshakadaniel <pyaesonehein.71299@gmail.com>
2025-02-05 16:15:44 +0800

    dialog update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/UI/Dialog";
+} from "@/components/ui/dialog";
 import { FcGoogle } from "react-icons/fc";
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
