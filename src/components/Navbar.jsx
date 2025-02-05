cafb66b2a09c6c63927bf73afe8a07d87525f113
pshakadaniel <pyaesonehein.71299@gmail.com>
2025-02-05 18:17:11 +0800

    popover upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover";
+} from "../components/UI/Popover";
 import { googleLogout } from "@react-oauth/google";
 import {
   Dialog,
